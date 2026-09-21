a1d615babfdadec70d8c3f063569cf6c4478b623
MarkOne-dev <u202419485@upc.edu.pe>
2026-09-21 11:04:19 -0500

    feat(i18n): install and configure i18next and react-i18next with TypeScript typings

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
+import "./i18n/config"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { ExternalLinkGuard } from "./components/external-link-guard.tsx"
